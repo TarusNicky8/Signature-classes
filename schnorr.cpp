ea9e4a7ca18d756ffd40a233373cfd9ea211d09b
User <User@MIKE>
2022-06-28 11:04:40 +0300

    Add project files.

---
+++ schnorr.cpp
@@ -0,0 +1,249 @@
+
+
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <cstring>
+
+#include "schnorr.h"
+#include "base64.h"
+
+CryptoKernel::Schnorr::Schnorr() {
+    ctx = schnorr_context_new();
+    key = musig_key_new(ctx);
+    pkey = key->pub;
+
+    if(key == NULL) {
+        throw std::runtime_error("Could not generate key pair");
+    }
+}
+
+CryptoKernel::Schnorr::~Schnorr() {
+    schnorr_context_free(ctx);
+    musig_key_free(key);
+}
+
+bool CryptoKernel::Schnorr::verify(const std::string& message,
+                                   const std::string& signature) {
+    const std::string decodedSignature = base64_decode(signature);
+
+    musig_sig* sig = NULL;
+    sig = reinterpret_cast<musig_sig*>(malloc(sizeof(musig_sig)));
+    if (sig == NULL) {
+        return false;
+    }
+    sig->s = NULL;
+    sig->R = NULL;
+
+    sig->s = BN_new();
+    if (sig->s == NULL) {
+        musig_sig_free(sig);
+        return false;
+    }
+
+    sig->R = EC_POINT_new(ctx->group);
+    if (sig->R == NULL) {
+        musig_sig_free(sig);
+        return false;
+    }
+
+    if (!BN_bin2bn((unsigned char*)decodedSignature.c_str(), 32, sig->s)) {
+        musig_sig_free(sig);
+        return false;
+    }
+
+    if (!EC_POINT_oct2point(
+        ctx->group,
+        sig->R,
+        (unsigned char*)decodedSignature.c_str() + 32,
+        33,
+        ctx->bn_ctx)) {
+        musig_sig_free(sig);
+        return false;
+    }
+
+    if (musig_verify(
+        ctx,
+        sig,
+        pkey,
+        (unsigned char*)message.c_str(),
+        message.size()) != 1) {
+        musig_sig_free(sig);
+        return false;
+    }
+
+    musig_sig_free(sig);
+
+    return true;
+}
+
+std::string CryptoKernel::Schnorr::signSingle(const std::string& message) {
+    if (key != NULL) {
+        musig_sig* sig;
+
+        if (musig_sign_single(
+            ctx,
+            &sig,
+            key,
+            (unsigned char*)message.c_str(),
+            message.size()) == 0) {
+            musig_sig_free(sig);
+
+            throw std::runtime_error("Could not sign message");
+        } else {
+            const unsigned int buf_len = 65;
+            std::unique_ptr<unsigned char> buf(new unsigned char[buf_len]);
+
+            if (BN_bn2binpad(sig->s, buf.get(), 32) != 32) {
+                musig_sig_free(sig);
+                throw std::runtime_error("Failed to encode s");
+            }
+
+            if (EC_POINT_point2oct(
+                    ctx->group,
+                    sig->R,
+                    POINT_CONVERSION_COMPRESSED,
+                    buf.get() + 32,
+                    33,
+                    ctx->bn_ctx) != 33) {
+                musig_sig_free(sig);
+                throw std::runtime_error("Failed to encode R");
+            }
+
+            const std::string returning = base64_encode(buf.get(), buf_len);
+
+            musig_sig_free(sig);
+
+            return returning;
+        }
+    } else {
+        return "";
+    }
+}
+
+std::string CryptoKernel::Schnorr::pubkeyAggregate(const std::set<std::string>& pubkeys) {
+    std::unique_ptr<musig_pubkey> keys[pubkeys.size()];
+
+    unsigned int i = 0;
+    for(const auto& p : pubkeys) {
+        const std::string decodedKey = base64_decode(p);
+
+        keys[i].reset(new musig_pubkey);
+
+        keys[i]->A = EC_POINT_new(ctx->group);
+
+        if(!EC_POINT_oct2point(
+                ctx->group,
+                keys[i]->A,
+                (unsigned char*)decodedKey.c_str(),
+                decodedKey.size(),
+                ctx->bn_ctx)) {
+            return "";
+        }
+
+        i++;
+    }
+
+    musig_pubkey* keysArr[pubkeys.size()];
+    i = 0;
+    for(const auto& p : keys) {
+        keysArr[i] = p.get();
+        i++;
+    }
+
+    musig_pubkey* pk;
+    if(musig_pubkey_aggregate(ctx, keysArr, &pk, pubkeys.size()) != 1) {
+        return "";
+    }
+
+    const unsigned int buf_len = 33;
+    std::unique_ptr<unsigned char> buf(new unsigned char[buf_len]);
+
+    if (EC_POINT_point2oct(
+            ctx->group,
+            pk->A,
+            POINT_CONVERSION_COMPRESSED,
+            buf.get(),
+            buf_len,
+            ctx->bn_ctx) != buf_len) {
+        return "";
+    }
+
+    musig_pubkey_free(pk);
+
+    const std::string returning = base64_encode(buf.get(), buf_len);
+
+    return returning;
+}
+
+std::string CryptoKernel::Schnorr::getPublicKey() {
+    if (key != NULL) {
+        const unsigned int buf_len = 33;
+        std::unique_ptr<unsigned char> buf(new unsigned char[buf_len]);
+
+        if (EC_POINT_point2oct(
+                ctx->group,
+                pkey->A,
+                POINT_CONVERSION_COMPRESSED,
+                buf.get(),
+                buf_len,
+                ctx->bn_ctx) != buf_len) {
+            return "";
+        }
+
+        const std::string returning = base64_encode(buf.get(), buf_len);
+
+        return returning;
+    } else {
+        return "";
+    }
+}
+
+std::string CryptoKernel::Schnorr::getPrivateKey() {
+    if (key != NULL) {
+        const unsigned int buf_len = 32;
+        std::unique_ptr<unsigned char> buf(new unsigned char[buf_len]);
+
+        if (BN_bn2binpad(key->a, buf.get(), buf_len) != buf_len) {
+            return "";
+        }
+
+        const std::string returning = base64_encode(buf.get(), buf_len);
+
+        return returning;
+    } else {
+        return "";
+    }
+}
+
+bool CryptoKernel::Schnorr::setPublicKey(const std::string& publicKey) {
+    const std::string decodedKey = base64_decode(publicKey);
+
+    if(!EC_POINT_oct2point(
+            ctx->group,
+            pkey->A,
+            (unsigned char*)decodedKey.c_str(),
+            decodedKey.size(),
+            ctx->bn_ctx)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool CryptoKernel::Schnorr::setPrivateKey(const std::string& privateKey) {
+    const std::string decodedKey = base64_decode(privateKey);
+
+    if(!BN_bin2bn(
+            (unsigned char*)decodedKey.c_str(),
+            (unsigned int)decodedKey.size(),
+            key->a)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool CryptoKernel::Schnorr::getStatus() {
+    return true;
+}
---
+++ schnorr.h
@@ -0,0 +1,102 @@
+
+
+#ifndef SCHNORR_H_INCLUDED
+#define SCHNORR_H_INCLUDED
+
+#include <string>
+#include <memory>
+#include <set>
+
+#include <cschnorr/multisig.h>
+#include <json/value.h>
+
+
+namespace CryptoKernel {
+
+/**
+* A mutable class which performs cryptography operations on Schnorr keypairs. It provides funtions to
+* generate keys, signing and verifying.
+*/
+class Schnorr {
+public:
+    /**
+    * Constructs a Schnorr object. Optionally generates a Schnorr keypair.
+    */
+    Schnorr();
+
+    /**
+    * Schnorr class deconstructor
+    */
+    ~Schnorr();
+
+    /**
+    * DEPRECATED: Returns the status of the Schnorr module
+    *
+    * @return the status of the Schnorr module. Always true.
+    */
+    bool getStatus();
+
+    /**
+    * Generates an Schnorr signature from the given message.
+    *
+    * @param message the message to sign, must be non-empty
+    * @return the Schnorr signature of the message and public key as base64_encode(s + R)
+    */
+    std::string signSingle(const std::string& message);
+
+    /**
+    * Verifies a that a given signature verifies the given message with the public key of this class
+    *
+    * @param message the message to verify, must be non-empty
+    * @param signature the signature to check against, must be non-empty
+    * @return true if the signature is correctly verified against the message and public key, false otherwise
+    */
+    bool verify(const std::string& message, const std::string& signature);
+
+    /**
+     * Aggregates a set of public keys in to an aggregate public key.
+     *
+     * @param pubkeys a set of pubkeys to be aggregated
+     * @return the aggregated pubkey if successful, empty string otherwise
+     */
+    std::string pubkeyAggregate(const std::set<std::string>& pubkeys);
+
+    /**
+    * Returns the public key of the instance
+    *
+    * @return the public key of the class encoded base64
+    */
+    std::string getPublicKey();
+
+    /**
+    * Returns the private key of the instance
+    *
+    * @return the private key of the class encoded base64
+    */
+    std::string getPrivateKey();
+
+    /**
+    * Sets the public key of the instance
+    *
+    * @param publicKey valid public key from another instance of Schnorr
+    * @return true if setting the key was successful, false otherwise
+    */
+    bool setPublicKey(const std::string& publicKey);
+
+    /**
+    * Sets the private key of the instance
+    *
+    * @param privateKey valid private key from another instance of Schnorr
+    * @return true if setting the key was successful, false otherwise
+    */
+    bool setPrivateKey(const std::string& privateKey);
+
+private:
+    musig_key* key;
+    musig_pubkey* pkey;
+    schnorr_context* ctx;
+};
+
+}
+
+#endif  // SCHNORR_H_INCLUDED
